eff5fff82e293bc1ce8e35454c128d96ad69563a
Sindre Skorpen <64530905+Westdespair@users.noreply.github.com>
2023-02-19 23:09:42 +0100

    Added fade in/out of visuals

--- Assets/Scripts/SeatTeleporter.cs
+++ Assets/Scripts/SeatTeleporter.cs
@@ -10,10 +10,11 @@ public class SeatTeleporter : MonoBehaviour {
     [SerializeField]
     [Tooltip("The player that will be teleported on activation")]
     private GameObject player;
+
+    private float maxAlpha = 0.6f;
     // Start is called before the first frame update
     void Start()
     {
-        ShowCollider();
     }
 
     // Update is called once per frame
@@ -23,20 +24,39 @@ public class SeatTeleporter : MonoBehaviour {
     }
 
     public void ShowCollider() {
-        SetMaterialAlpha(0.6f);
+        StopAllCoroutines();
+        GetComponent<MeshRenderer>().enabled = true;
+        StartCoroutine(GraduallyChangeColliderAlpha(maxAlpha, 0.3f));
     }
+
+
     public void HideCollider() {
-        SetMaterialAlpha(0f);
-        GetComponent<MeshRenderer>().enabled = false;
+        StopAllCoroutines();
+        StartCoroutine(FadeToDisabledCollider());
     }
 
     private void SetMaterialAlpha(float alpha) {
         GetComponent<MeshRenderer>().enabled = true;
         Material seatMaterial = GetComponent<Renderer>().material;
-        seatMaterial.color = new Color(seatMaterial.color.r, seatMaterial.color.g, seatMaterial.color.b, alpha);
+        Color materialColor = seatMaterial.color;
+        materialColor.a = alpha;
+        seatMaterial.color = materialColor;
     }
 
-    
+    private IEnumerator FadeToDisabledCollider() {
+        yield return StartCoroutine(GraduallyChangeColliderAlpha(0, 0.3f));
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private IEnumerator GraduallyChangeColliderAlpha(float targetAlpha, float time) {
+        float timeElapsed = 0;
+        float startingAlpha = GetComponent<Renderer>().material.color.a;
+        while(timeElapsed < time) {
+            SetMaterialAlpha(Mathf.Lerp(startingAlpha, targetAlpha, timeElapsed / time));
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
 
     public void TeleportToSeat() {
         player.transform.position = transform.position;
