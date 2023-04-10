bbbcdbdab7aca38b26499ad2a7066d2650fcea89
StonedStonar <steinarhjellem@gmail.com>
2023-04-10 23:53:18 +0200

    Added basic methods for the object-based-color-coding

--- Assets/Scripts/ObjectBasedColorCoding.cs
+++ Assets/Scripts/ObjectBasedColorCoding.cs
@@ -2,21 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectBasedColorCoding : MonoBehaviour, TrackableObserver
+[RequireComponent(typeof(TrackableObjectController))]
+public class ObjectBasedColorCoding : MonoBehaviour
 {
+    [SerializeField, Tooltip("The prefab for the box")]
+    private GameObject prefab;
 
-    public void UpdateAverageFixationDuration(float averageFixationDuration)
-    {
-        throw new System.NotImplementedException();
+    [SerializeField, Tooltip("The colored part of the object")]
+    private GameObject colorObject;
+
+
+    public void SetValues(Metric metric, float total) {
+        if (colorObject == null) {
+            MakeColorObject();
+        }
+        GazeData gazeData = GetComponent<TrackableObjectController>().GetCurrentGazeData();
+        float value = metric == Metric.FIXATIONS ? gazeData.GetFixations() : gazeData.GetFixationDuration();
+        float prosentage = value / total;
+        if (prosentage > 0.5f)
+        {
+            colorObject.GetComponent<Material>().color = Color.green;
+        }
+        else if (prosentage > 0.25f)
+        {
+            colorObject.GetComponent<Material>().color = Color.blue;
+        }
+        else {
+            colorObject.GetComponent<Material>().color = Color.red;
+        }
     }
 
-    public void UpdateFixationDuration(float fixationDuration)
-    {
-        throw new System.NotImplementedException();
+    public void MakeColorObject() {
+        this.colorObject = Instantiate(prefab);
+        this.colorObject.transform.SetParent(this.transform);
+        this.colorObject.transform.localScale.Set(transform.localScale.x + 0.1f, transform.localScale.y + 0.1f, transform.localScale.z + 0.1f);
     }
 
-    public void UpdateFixations(int fixations)
-    {
-        throw new System.NotImplementedException();
+    /// <summary>
+    /// Hides the object 
+    /// </summary>
+    public void HideObject() {
+        if (colorObject != null) {
+            colorObject.SetActive(false);
+        }
     }
 }
