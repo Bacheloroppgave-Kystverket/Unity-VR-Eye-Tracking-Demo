9178725d761a97af6086b5bbb165bbcbe3882260
Sindre Skorpen <64530905+Westdespair@users.noreply.github.com>
2023-04-21 13:56:55 +0200

    Added simple open and close animation, set up scene
    Set up UI scene for a small demo of the menu

--- Assets/Scripts/HandUIUtilities.cs
+++ Assets/Scripts/HandUIUtilities.cs
@@ -12,17 +12,22 @@ public class HandUIUtilities : MonoBehaviour
 
     [SerializeField]
     private GameObject menu;
+
     [SerializeField]
     private Vector3 openPosition;
+
     [SerializeField]
     private Vector3 closedPosition;
+
     [SerializeField]
     private float toggleTime;
     private bool isOpen;
+    private Vector3 initialScale;
     // Start is called before the first frame update
     void Start()
     {
         isOpen = menu.activeSelf;
+        openPosition = menu.transform.position;
     }
 
     // Update is called once per frame
@@ -35,13 +40,13 @@ public class HandUIUtilities : MonoBehaviour
         if(isOpen) {
             StartCoroutine(DisableAndCloseMenu());
         } else {
-            EnableMenu();
+            StartCoroutine(EnableAndOpenMenu());
         }
     }
 
     public void EnableMenu() {
         Debug.Log("Opening hand menu.");
-        menu.transform.position = openPosition;
+       // menu.transform.position = openPosition;
         isOpen = true;
         menu.SetActive(isOpen);
     }
@@ -52,17 +57,31 @@ public class HandUIUtilities : MonoBehaviour
         menu.SetActive(isOpen);
     }
 
-    IEnumerator DisableAndCloseMenu() {
-        StartCoroutine(LerpToPosition(leftHandIconPosition.transform.position, toggleTime));
+    private IEnumerator DisableAndCloseMenu() {
+        StartCoroutine(LerpToPosition(openPosition, leftHandIconPosition.transform.position, toggleTime));
         yield return new WaitForSeconds(toggleTime);
         DisableMenu();
     }
 
-    IEnumerator LerpToPosition(Vector3 newPosition, float seconds) {
+    private IEnumerator EnableAndOpenMenu() {
+        EnableMenu();
+        StartCoroutine(LerpToPosition(leftHandIconPosition.transform.position,openPosition, toggleTime));
+        yield return null;
+    }
+    
+    IEnumerator LerpVector3OverTime(Vector3 initialVector, Vector3 targetRotation, float seconds) {
         float timeElapsed = 0;
-        openPosition = menu.transform.position;
-        while (menu.transform.position != newPosition && timeElapsed <1) {
-            menu.transform.position = Vector3.Lerp(openPosition, newPosition, timeElapsed/seconds);
+        while (initialVector != targetRotation && timeElapsed < 1) {
+            menu.transform.position = Vector3.Lerp(initialVector, targetRotation, timeElapsed / seconds);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+    IEnumerator LerpToPosition(Vector3 fromPosition, Vector3 toPosition, float seconds) {
+        float timeElapsed = 0;
+        //openPosition = menu.transform.position;
+        while (menu.transform.position != toPosition && timeElapsed <1) {
+            menu.transform.position = Vector3.Lerp(fromPosition, toPosition, timeElapsed/seconds);
             timeElapsed += Time.deltaTime;
             yield return null;
         }
